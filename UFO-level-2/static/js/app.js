efbfb89df4e36b6f6a4a5ea8b27fe773c4e0d2f3
tdoanUNCG <48730552+tdoanUNCG@users.noreply.github.com>
2020-03-03 18:16:38 -0500

    mmmmmmm...

---
+++ UFO-level-2/static/js/app.js
@@ -0,0 +1,41 @@
+// from data.js
+var tableData = data;
+// get reference from tbody
+var tbody = d3.select('tbody');
+// get reference from button
+var button = d3.select('#filter-btn');
+
+// YOUR CODE HERE!
+// if filter is applied
+button.on('click',function(){
+    //select input element and get raw HTML node
+    var inputElement = d3.select('#datetime');
+    //get value property of input element
+    var inputValue = inputElement.property('value');
+    console.log(inputValue);
+    //console.log(data);
+    var filteredData = data.filter(data=>data.datetime===inputValue);
+    //console.log(filteredData);
+    //remove any children row
+    tbody.html('');
+    //replace all data with filtered data
+    filteredData.forEach(function(fData){
+        //console.log(fData);
+        var row = tbody.append('tr');
+        Object.entries(fData).forEach(function([key,value]){
+            //console.log(key,value);
+            var cell = row.append('td');
+            cell.text(value);
+        });
+    });
+});
+
+data.forEach(function(sighting_data){
+    //console.log(sighting_data);
+    var row = tbody.append('tr');
+    Object.entries(sighting_data).forEach(function([key,value]){
+        //console.log(key,value);
+        var cell = row.append('td');
+        cell.text(value);
+    });
+});
